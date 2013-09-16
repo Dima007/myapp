cbff5490b096e905562418dbb478c99439e1fb28
Dima Rymar <dimarymar@gmail.com>
2013-09-16 07:08:12 +0300

    fix test user_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Myapp::Application.routes.draw do
+  get "users/new"
   root 'static_pages#home'
+
+  get '/signup'     => 'users#new'
   get '/help'       => 'static_pages#help'
   get '/about'      => 'static_pages#about'
   get '/contact'    => 'static_pages#contact'
