afd69a0139a68ef8f1400d87f617bb67de5cbb05
Dima Rymar <dimarymar@gmail.com>
2013-09-19 01:48:13 +0300

    add flash notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to root_path
+  		flash[:notice] = "Welcome to the Myapp"
+  		redirect_to @user
   	else
   		render 'new'
   	end
