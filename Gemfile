b82ac8b151abd4b4514dd8e8e64870f3c2d9d5e1
Dima Rymar <dimarymar@gmail.com>
2013-09-19 06:21:33 +0300

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :test do
   	gem 'rb-inotify'
   	gem 'libnotify'
   	gem 'factory_girl_rails'
+  	gem 'cucumber-rails', :require => false
 end
 
 group :production do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,28 @@
 module SessionsHelper
+
+	#Установка remember_token в куки браузера
+	def sign_in(user)
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+
+	#Является ли пользователь вошедгим
+	def signed_in?
+		!current_user.nil?
+	end
+
+	#сеттер для current_user
+	def current_user=(user)
+		@current_user = user		
+	end
+
+	#Поиск текущего пользователя
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+
+	def sign_out
+		self.current_user = nil
+		cookies.delete(:remember_token)
+	end
 end
